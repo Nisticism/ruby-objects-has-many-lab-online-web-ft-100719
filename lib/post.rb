5043ce58f92d74a1b9975b8f68032f0e045edc71
Hans Foster <fosterhans@gmail.com>
2019-11-07 20:48:22 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,10 +1,20 @@
-class Artist 
+class Post
   
-  attr_accessor :name
+  attr_accessor :title, :author
   
-  def initialize(name)
-    @name = name 
-    
+  @@all = []
+  
+  def initialize(title)
+    @title = title
+    save 
+  end
+  
+  def save
+    @@all < self 
+  end
+  
+  def self.all 
+    @@all 
   end
   
 end
