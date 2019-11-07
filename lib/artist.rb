eae55720f02f1821ed11e5986c028de003279a96
Hans Foster <fosterhans@gmail.com>
2019-11-07 21:11:23 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,30 +2,35 @@ class Artist
   
   attr_accessor :name, :songs, :genre
   
-  @@all_songs = []
-  
-  def initialize(genre)
+  def initialize(name, genre)
+    @@all = []
+    @@songs = []
     @genre = genre
-    @songs = []
   end
   
   def songs_all
     @songs
   end
   
+  def self.name
+    @name
+  end
+  
   def add_song(song)
     song.artist = self.name
     @songs << song
+    @@all << song
   end
   
   def add_song_by_name(name)
     new_song = Song.new(name)
     new_song.artist = self.name
     @songs << new_song
+    @@all << song
   end
   
   def self.song_count
-    @songs.size
+    @@all.size 
   end
     
   
