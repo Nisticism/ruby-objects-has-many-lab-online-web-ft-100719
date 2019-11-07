aee54c0f7270d632b3a1ff6121b28dcad8ad339f
Hans Foster <fosterhans@gmail.com>
2019-11-07 20:52:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,16 +2,17 @@ class Song
   
   attr_accessor :name, :artist, :genre
   
-  @@all = []
+  @@all
   
   def initialize(name, genre)
+    @@all = []
     @title = title
     @genre = genre
     save 
   end
   
   def save
-    @@all < self 
+    @@all << self 
   end
   
   def self.all 
